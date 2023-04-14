7e5e5fcc135023dbff0e8832a98102437c90ea29
blacksev <bcyfkne@outlook.com>
2023-04-14 17:59:51 +0800

    add debug log

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.all("/", async (req, res) => {
     console.log(response);
     if (response && response.choices && response.choices.length > 0) {
       const replyMessage = response.choices[0].message.content;
+      console.log(replyMessage)
       return res.send({
         ToUserName: FromUserName,
         FromUserName: ToUserName,
