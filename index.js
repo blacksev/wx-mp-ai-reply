c891b4955e24e5b2d537c4219466748194bea730
blacksev <bcyfkne@outlook.com>
2023-04-15 00:22:17 +0800

    support timeout

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const path = require("path");
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const got = require('got');
+const got = require("got");
 const logger = morgan("tiny");
 
 const app = express();
-app.use(bodyParser.raw())
-app.use(bodyParser.json({}))
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.raw());
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger);
 
 const messageStore = {};
@@ -19,57 +19,49 @@ function timeout(ms) {
 
 // 处理消息
 app.all("/", async (req, res) => {
-  console.log('消息推送', JSON.stringify(req.body))
+  console.log("消息推送", JSON.stringify(req.body));
   const { ToUserName, FromUserName, MsgType, Content, CreateTime, MsgId } =
     req.body;
-  if (MsgType === 'text') {
+  if (MsgType === "text") {
     if (messageStore[MsgId] === undefined) {
-      messageStore[MsgId] = '';
-      try {
-        const response = await got
-          .post("https://exapi-chat.zecoba.cn/v1/chat/completions", {
-            headers: {
-              Authorization: `Bearer ${process.env.ZECOBA_API_KEY}`,
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              model: "gpt-3.5-turbo",
-              messages: [{ role: "user", content: Content }],
-              temperature: 0.7,
-            }),
-          })
-          .json();
-
-        // console.log(JSON.stringify(response));
-        if (response && response.choices) {
-          const replyMessage = response.choices[0].message.content;
-          console.log(replyMessage);
-          messageStore[MsgId] = replyMessage;
-          return res.send({
-            ToUserName: FromUserName,
-            FromUserName: ToUserName,
-            CreateTime: CreateTime,
-            MsgType: "text",
-            Content: messageStore[MsgId],
-          });
-        }
-      } catch (error) {
-        console.error(error);
+      messageStore[MsgId] = "";
+      got
+        .post("https://exapi-chat.zecoba.cn/v1/chat/completions", {
+          headers: {
+            Authorization: `Bearer ${process.env.ZECOBA_API_KEY}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: Content }],
+            temperature: 0.7,
+          }),
+        })
+        .json()
+        .then((response) => {
+          // console.log(JSON.stringify(response));
+          if (response && response.choices) {
+            const replyMessage = response.choices[0].message.content;
+            console.log(replyMessage);
+            messageStore[MsgId] = replyMessage;
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+    for (let i = 0; i < 100; i++) {
+      if (messageStore[MsgId]) {
+        delete messageStore[MsgId];
+        return res.send({
+          ToUserName: FromUserName,
+          FromUserName: ToUserName,
+          CreateTime: CreateTime,
+          MsgType: "text",
+          Content: messageStore[MsgId],
+        });
       }
-    } else { 
-      for (let i = 0; i < 100; i++) {
-        if (messageStore[MsgId]) {
-          delete messageStore[MsgId];
-          return res.send({
-            ToUserName: FromUserName,
-            FromUserName: ToUserName,
-            CreateTime: CreateTime,
-            MsgType: "text",
-            Content: messageStore[MsgId],
-          });
-        }
-        await timeout(50);
-      }
+      await timeout(50);
     }
   } else {
     res.send("success");
